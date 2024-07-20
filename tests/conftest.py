2d4190e9b5cd21a99f00ab8774d5d2aebcb5e9a9
Anna Fedorova <anna.fedorova.testing@gmail.com>
2024-07-20 23:12:35 +0300

    добавлен аттач видео

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
-def setting_browser():
+def setting_browser(request):
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
@@ -34,5 +34,6 @@ def setting_browser():
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
